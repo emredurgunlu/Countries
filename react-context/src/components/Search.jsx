66fe612bb15958a61232b2195dc5ba554c187a32
emre durgunlu <emredurgunlu@gmail.com>
2023-12-14 12:26:35 +0300

    update

--- react-context/src/components/Search.jsx
+++ react-context/src/components/Search.jsx
@@ -4,31 +4,31 @@ import { useEffect } from "react";
 import { useContext } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 import style from "../scss/search.module.scss";
-let duplicateSortedData;
+
 const Search = () => {
   const { data, setData } = useContext(GlobalContext);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get("https://restcountries.com/v3.1/all");
-        console.log("the data is gotten from the api");
-        let sortedData = response.data.sort((a, b) => {
-          // If you are sure that all the chars are lowerCase or upperCase you can get rid of ".toLowerCase()" for below
-          // if it's not a object, just a simple array, there is no need nameA,nameB, just use  return a.localeCompare(b);
-          var nameA = a.name.common.toLowerCase();
-          var nameB = b.name.common.toLowerCase();
-          return nameA.localeCompare(nameB);
-        });
-        // setData(response.data);
-        setData(sortedData);
-        duplicateSortedData = sortedData;
-      } catch (error) {
-        console.error(error);
-      }
+  let duplicateSortedData;
+  async function fetchData() {
+    try {
+      const response = await axios.get("https://restcountries.com/v3.1/all");
+      console.log("the data is gotten from the api");
+      let sortedData = response.data.sort((a, b) => {
+        // If you are sure that all the chars are lowerCase or upperCase you can get rid of ".toLowerCase()" for below
+        // if it's not a object, just a simple array, there is no need nameA,nameB, just use  return a.localeCompare(b);
+        var nameA = a.name.common.toLowerCase();
+        var nameB = b.name.common.toLowerCase();
+        return nameA.localeCompare(nameB);
+      });
+      // setData(response.data);
+      setData(sortedData);
+      duplicateSortedData = sortedData;
+    } catch (error) {
+      console.error(error);
     }
+  }
+  useEffect(() => {
     fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   console.log(data);
   const handleChange = (e) => {
--- react-props/src/App.js
+++ react-props/src/App.js
@@ -3,30 +3,31 @@ import axios from "axios";
 import Countries from "./components/Countries";
 
 import AppStyle from "./scss/app.module.scss";
-let duplicateSortedData;
+
 function App() {
   const [data, setData] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get("https://restcountries.com/v3.1/all");
-        console.log("the data is gotten from the api");
-        let sortedData = response.data.sort((a, b) => {
-          // If you are sure that all the chars are lowerCase or upperCase you can get rid of ".toLowerCase()" for below
-          // if it's not a object, just a simple array, there is no need nameA,nameB, just use  return a.localeCompare(b);
-          var nameA = a.name.common.toLowerCase();
-          var nameB = b.name.common.toLowerCase();
-          return nameA.localeCompare(nameB);
-        });
-        // setData(response.data);
-        setData(sortedData);
-        duplicateSortedData = sortedData;
-      } catch (error) {
-        console.error(error);
-      }
+  let duplicateSortedData;
+  async function fetchData() {
+    try {
+      const response = await axios.get("https://restcountries.com/v3.1/all");
+      console.log("the data is gotten from the api");
+      let sortedData = response.data.sort((a, b) => {
+        // If you are sure that all the chars are lowerCase or upperCase you can get rid of ".toLowerCase()" for below
+        // if it's not a object, just a simple array, there is no need nameA,nameB, just use  return a.localeCompare(b);
+        var nameA = a.name.common.toLowerCase();
+        var nameB = b.name.common.toLowerCase();
+        return nameA.localeCompare(nameB);
+      });
+      // setData(response.data);
+      setData(sortedData);
+      duplicateSortedData = sortedData;
+    } catch (error) {
+      console.error(error);
     }
+  }
+  useEffect(() => {
     fetchData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   //  data && console.log("emre", data); you need to do data && before the console log here
 
